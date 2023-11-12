e1365e71c323fce020caaf1ec9bfe48d07a80094
DJAMWAGES <djamil@wag3s.io>
2023-11-12 12:34:11 +0100

    gros commit pour header

--- app.config.ts
+++ app.config.ts
@@ -1,31 +1,44 @@
 export default defineAppConfig({
   docus: {
     title: 'Wag3s',
-    description: 'Streamline Your Finances and HR Management with Ease.',
+    description: 'The best place to start your documentation.',
+    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
+      github: 'CallixteBailly/wag3s_docs',
+      youtube: 'nuxt_js',
     },
     github: {
-      dir: '.starters/default/content',
+      dir: '',
       branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
+      repo: 'wag3s_docs',
+      owner: 'CallixteBailly',
       edit: true
     },
+    layout: 'default',
     aside: {
-      level: 0,
-      collapsed: false,
+      level: 1,
+      collapsed: true,
       exclude: []
     },
     main: {
       padded: true,
       fluid: true
+    },
+    footer: {
+      credits: {
+        icon: '',
+        text: 'powered by Wag3s',
+        href: '',
+      },
+      textLinks: [
+        {
+          text: '',
+          href: '',
+          target: '_blank',
+          rel: 'noopener'
+        }
+      ],
     },
     header: {
       logo: true,
@@ -35,4 +48,3 @@ export default defineAppConfig({
     }
   }
 })
- 
---
+++ components/CustomHeader.vue
@@ -0,0 +1,86 @@
+<template>
+    <header class="custom-header">
+      <div class="logo-container">
+        <!-- Add your logo component here -->
+        <img src="/img/smlogo.png" alt="Logo" class="logo" />
+      </div>
+  
+      <div class="navbar">
+        <!-- Add your navbar links or components here -->
+        <router-link to="/">Home</router-link>
+        <router-link to="/docs/">Docs</router-link>
+        <!-- Add more links as needed -->
+      </div>
+  
+      <div class="social-links">
+        <!-- Add your social links components here -->
+        <a href="https://twitter.com/nuxt_js" target="_blank" rel="noopener">
+          <img src="/path/to/twitter-icon.png" alt="Twitter" class="social-icon" />
+        </a>
+        <a href="https://github.com/CallixteBailly/wag3s_docs" target="_blank" rel="noopener">
+          <img src="/path/to/github-icon.png" alt="GitHub" class="social-icon" />
+        </a>
+        <!-- Add more social links as needed -->
+      </div>
+  
+      <!-- Add your custom button -->
+      <button @click="redirectToAnotherWebsite">Visit Another Website</button>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      redirectToAnotherWebsite() {
+        window.location.href = 'https://example.com';
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your custom styles for the header */
+  .custom-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    background-color: #f8f9fa; /* Add your desired background color */
+    border-bottom: 1px solid #e9ecef; /* Add your desired border color */
+  }
+  
+  .logo-container {
+    /* Add styles for the logo container */
+  }
+  
+  .logo {
+    /* Add styles for the logo */
+    max-width: 100px; /* Adjust the max-width as needed */
+    height: auto;
+  }
+  
+  .navbar {
+    /* Add styles for the navbar links */
+    display: flex;
+    gap: 20px;
+  }
+  
+  .router-link {
+    /* Add styles for the router links */
+    text-decoration: none;
+    color: #007bff; /* Add your desired link color */
+  }
+  
+  .social-links {
+    /* Add styles for the social links */
+    display: flex;
+    gap: 10px;
+  }
+  
+  .social-icon {
+    /* Add styles for the social icons */
+    width: 20px; /* Adjust the width as needed */
+    height: 20px; /* Adjust the height as needed */
+  }
+  </style>
+  
