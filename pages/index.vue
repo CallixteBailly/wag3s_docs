8edebb2d126de36971e3eb59641ffc70d7dba118
DJAMWAGES <djamil@wag3s.io>
2023-11-15 17:32:13 +0100

    modif index

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,37 @@
 <template>
   <div class="snap-container">
     <section class="snap-section" id="blockh">
-      <div class="content-center-horizontal content-center-vertical">
+      <div class="content-center">
         <Blockh/>
       </div>
     </section>
     <br>
     <section class="snap-section" id="principles">
-      <div class="content-center-horizontal content-center-vertical">
+      <div class="content-center">
         <Principles/>
       </div>
     </section>
     <br>
     <section class="snap-section" id="wagesfolio">
-      <div class="content-center-horizontal content-center-vertical">
+      <div class="content-center">
         <Wagesfolio/>
       </div>
     </section>
     <br>
     <section class="snap-section" id="wageledger">
-      <div class="content-center-horizontal content-center-vertical">
+      <div class="content-center">
         <Wageledger/>
       </div>
     </section>
     <br>
     <section class="snap-section" id="wageshr">
-      <div class="content-center-horizontal content-center-vertical">
+      <div class="content-center">
         <Wageshr/>
       </div>
     </section>
     <br>
     <section class="snap-section" id="upcoming">
-      <div class="content-center-horizontal content-center-vertical">
+      <div class="content-center">
         <Upcoming/>
       </div>
     </section>      
@@ -49,16 +49,11 @@
     scroll-snap-align: start;
     min-height: 100vh;
     display: flex;
-    flex-direction: column; /* Ajout de la direction de colonne pour garder l'alignement vertical */
-    max-width: 1200px; /* Limite la largeur à 1200px */
-    margin: 0 auto; /* Centre la section horizontalement */
+    justify-content: center;
+    align-items: center;
 }
 
-.content-center-horizontal {
+.content-center {
     text-align: center; /* pour le centrage horizontal du texte et des éléments en ligne */
-}
-
-.content-center-vertical {
-    margin-top: 0; /* Ajustement pour garder l'alignement vertical en haut */
 }
 </style>
