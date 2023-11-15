91a7146ea79a98dd979e5d2fad2264a818f21b1c
bailly.nthony <bailly.nthony@gmail.com>
2023-11-15 22:46:16 +0100

    Add condtion for light switch image

--- components/content/Sections/Wagesfolio.vue
+++ components/content/Sections/Wagesfolio.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="section-header">
-        <img src="/img/iconWagefolio.png" alt="WageFolio Icon" class="icon" />
+    <div class="section-header">
+        <img src="/iconWagefolio.png" alt="WageFolio Icon" class="icon" />
         <div class="title-container">
             <h1 class="gtitle">Wagesfolio - Your Personalized Crypto Dashboard</h1>
         </div>
@@ -8,24 +8,26 @@
 
     <div class="image-section">
         <div class="greenellipse left-ellipse"></div>
-        <img src="/public/Wagesfolio.png" class="image-left" />
-        <img src="/public/cardwf.png" class="image-right" />
+        <img :src="colorMode.value === 'light' ? '/Wagesfolio_light.png' : '/Wagesfolio.png'"
+            class="image-left" />
+        <img :src="colorMode.value === 'light' ? '/cardwf_light.png' : '/cardwf.png'" class="image-right" />
     </div>
     <br>
     <p class="bold-text">But that's just the tip of the iceberg. Wagesfolio offers a plethora of additional features</p>
-<br>
+    <br>
     <GridCustom>
-    <CustomCard2 color="rgba(51, 255, 251, 0.929)" title="Panic Sell"
-      description="Sell your cryptocurrencies in just a few clicks when needed." image="/img/logoZen.png" />
-    <CustomCard2 color="rgba(51, 255, 251, 0.929)" title="NFTbook"
-      description="Organize and manage your assets seamlessly." image="/img/iconNFTb.png" />
-    <CustomCard2 color="rgba(51, 255, 251, 0.929)" title="Backup"
-      description="Safeguard your precious tokens with ease, ensuring they're protected against unexpected events"
-      image="/img/iconbacup.png" />
-  </GridCustom>
-
+        <CustomCard2 color="rgba(51, 255, 251, 0.929)" title="Panic Sell"
+            description="Sell your cryptocurrencies in just a few clicks when needed." image="/img/logoZen.png" />
+        <CustomCard2 color="rgba(51, 255, 251, 0.929)" title="NFTbook"
+            description="Organize and manage your assets seamlessly." image="/img/iconNFTb.png" />
+        <CustomCard2 color="rgba(51, 255, 251, 0.929)" title="Backup"
+            description="Safeguard your precious tokens with ease, ensuring they're protected against unexpected events"
+            image="/img/iconbacup.png" />
+    </GridCustom>
 </template>
-  
+<script setup>
+const colorMode = useColorMode()
+</script>
 <style>
 .section-header {
     display: flex;
@@ -33,9 +35,11 @@
     justify-content: center;
     margin-bottom: 30px;
 }
+
 .title-container {
-    flex: 1; /* Utilise le reste de l'espace disponible */
+    flex: 1;
 }
+
 .icon {
     width: 40px;
     margin-right: 10px;
@@ -51,7 +55,8 @@
     position: relative;
     display: flex;
     align-items: center;
-    justify-content: space-around; /* Ajusté pour une meilleure répartition de l'espace */
+    justify-content: space-around;
+    /* Ajusté pour une meilleure répartition de l'espace */
 }
 
 .greenellipse {
@@ -69,10 +74,14 @@
     right: -120px;
 }
 
-.image-left, .image-right {
-    width: auto; /* Ajusté pour la taille de l'image */
-    max-width: 100%; /* Ajusté pour une meilleure visibilité */
-    height: auto; /* Assure que l'image garde ses proportions */
+.image-left,
+.image-right {
+    width: auto;
+    /* Ajusté pour la taille de l'image */
+    max-width: 100%;
+    /* Ajusté pour une meilleure visibilité */
+    height: auto;
+    /* Assure que l'image garde ses proportions */
 }
 
 .bold-text {
@@ -81,10 +90,10 @@
     font-weight: bold;
     color: #4DA8A2;
 }
+
 /* Styles spécifiques au mode clair */
 
 .light .bold-text {
-  color: #555; /* Une couleur plus douce pour le texte en mode clair */
-}
-
-</style>
+    color: #555;
+    /* Une couleur plus douce pour le texte en mode clair */
+}</style>
